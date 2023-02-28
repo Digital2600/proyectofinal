b99e17861bffe9656622a1c4b7dface3302895e0
Digital2600 <jagtecnicopc@gmail.com>
2023-02-28 20:00:12 -0300

    Segunda confirmacion

---
+++ js/funciones.js
@@ -0,0 +1,10 @@
+
+    document.getElementById('boton').onclick = function (){
+        document.getElementById('boton').innerHTML ="38134004246"
+    }/*
+    document.getElementById('boton').addEventListener('click', function(){
+       document.body.style.colorbackgroundColor = 'brown';
+    })
+    /*document.addEventListener("click",function() {*/
+
+    
